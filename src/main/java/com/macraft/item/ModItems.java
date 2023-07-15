8ae5bd093e0780415c241134e9aef0cd2eb13f26
Anatoliy <speranskii.1999@mail.ru>
2023-07-15 22:27:49 +0600

    Added new item Manas Crystal

--- src/main/java/com/macraft/item/ModItems.java
+++ src/main/java/com/macraft/item/ModItems.java
@@ -1,19 +1,28 @@
 package com.macraft.item;
 
 import com.macraft.MaCraft;
-
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 
 public class ModItems{
-    
+    public static final Item MANACRYSTAL = registerItem("manacrystal", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngridientItemGroup(FabricItemGroupEntries entries){
+        entries.add(MANACRYSTAL);
+    }
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM,  new Identifier(MaCraft.MOD_ID, name), item);
     }
     public static void registerModItems(){
         MaCraft.LOGGER.info("registeringmoditems" + MaCraft.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngridientItemGroup );
     }
     
 }
