93c442e86f5a0cacf45b19f1de80d6a321cee823
Anatoliy <speranskii.1999@mail.ru>
2023-07-22 21:35:17 +0600

    Fix for textures

---
+++ src/main/java/com/macraft/MaCraftDataGenerator.java
@@ -0,0 +1,11 @@
+package com.macraft;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MaCraftDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
